260043881a90dd494d2c0a35063b8c2b62f0fe68
Sabelo Thabethe <thbsab001@myuct.ac.za>
2020-07-25 15:36:20 +0200

    add logo image and logout user

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Image } from "react-bootstrap";
+
+import firebaseApp from "../config/firebase";
 
 export default function Menu() {
-    return (
+  const [user, setUser] = useState();
 
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Logo</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="/register">Create Account</Nav.Link>
-                <Nav.Link href="/signin">Login</Nav.Link>
-                <Nav.Link href="/signout">Log Out</Nav.Link>
-                <Nav.Link href="/uploud">Uploud Image</Nav.Link>
-            </Nav>
-        </Navbar>
+  useEffect(() => {
+    firebaseApp.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
 
-    )
+  const handleSignOut = (e) =>{
+    firebaseApp.auth().signOut().then(()=>{
+        setUser(null)
+    })
+  }
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">
+        <Image
+          src={process.env.PUBLIC_URL + "/logo.png"}
+          width={50}
+          height={50}
+        />
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        {user ? (
+          <>
+            <Nav.Link href="/signin" onClick={handleSignOut}>Log Out</Nav.Link>
+            <Nav.Link href="/uploud">Uploud Image</Nav.Link>
+          </>
+        ) : (
+          <>
+            <Nav.Link href="/register">Create Account</Nav.Link>
+            <Nav.Link href="/signin">Login</Nav.Link>
+          </>
+        )}
+      </Nav>
+    </Navbar>
+  );
 }
