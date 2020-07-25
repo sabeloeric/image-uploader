1b46a1c6baec1a590954d70bd919e51c6ea7c9ca
Sabelo Thabethe <thbsab001@myuct.ac.za>
2020-07-25 18:47:07 +0200

    add build folder to git

---
+++ build/precache-manifest.3c86abbdcb64253016d69cd26d50359f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186acd90d1d1eab363b8855e70bdd740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ed7829fb0557aa5a18e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a28eeb9f9c773c240d60",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ed7829fb0557aa5a18e",
+    "url": "/static/js/2.0ff00871.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.0ff00871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28eeb9f9c773c240d60",
+    "url": "/static/js/main.816b5e6c.chunk.js"
+  },
+  {
+    "revision": "fde99981696363e6039e",
+    "url": "/static/js/runtime-main.3be1564d.js"
+  }
+]);
